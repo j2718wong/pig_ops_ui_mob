4e04c37b7d417902bcc1f40f5a59ea8b26146285
Jack Wong <j2718wong@gmail.com>
2026-06-10 14:44:35 +0800

    minor fix

--- src/static/js/pages/production/history/page_prod_not_pregnant_list.js
+++ src/static/js/pages/production/history/page_prod_not_pregnant_list.js
@@ -108,7 +108,7 @@ export function PageProdNotPregnantList(input_settings){
         
         const helper = navigation.managerTranslations.translationHelper;
 
-        page_title      = helper.getSimpleTranslation('navigation.nav_links.Production5') || page_title;
+        page_title      = helper.getSimpleTranslation('navigation.nav_links.Production6') || page_title;
         page_info       = helper.getSimpleTranslation('page_info.not_pregnant') || page_info;
         
         
